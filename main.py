9a101ef00613712883912a2f6af4484deb9fa129
ebraamemad <beboe668@gmail.com>
2025-05-31 19:54:27 +0300

    upd

--- main.py
+++ main.py
@@ -75,4 +75,4 @@ async def predict(file: UploadFile = File(...)):
 #if __name__ == "__main__":
  #   import uvicorn
   #  uvicorn.run(app, host="0.0.0.0", port=8000)
-print("device", device)
+#print("device", device)
